68b7b8f0f58163bf4bcc4c5c6cfee72bec414a90
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-02-01 16:40:29 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,21 @@
-/**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
- */
 public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+
+        int N = Integer.parseInt(args[0]);
+        int sum = 1;  // First divisor is 1
+        String divisors = "1";
+
+        for (int i = 2; i <= N / 2; i++) {
+            if (N % i == 0) {
+                sum += i;
+                divisors += " + " + i;
+            }
+        }
+
+        if (sum == N) {
+            System.out.println(N + " is a perfect number since " + N + " = " + divisors);
+        } else {
+            System.out.println(N + " is not a perfect number");
+        }
+    }
 }
