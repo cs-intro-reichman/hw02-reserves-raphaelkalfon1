51d2e23a9329473514a02a594f76fe7b79787906
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-02-01 16:37:56 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,18 @@
-/**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
- */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
-	}
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 10);
+        System.out.print(x + " ");
+
+        int y = x + 1;
+
+        do {
+            y = (int) (Math.random() * 10);
+
+            if (y >= x) {
+                System.out.print(y + " ");
+                x = y; // Update x to the current value of y
+                y = (int) (Math.random() * 10);
+            } 
+        } while (x <= y); 
+    }
 }
