f84ca7c2c326354674f9462af09ea7f600855de5
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-02-01 16:35:13 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,29 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        // Size of the board 
+        int x = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < x; i++) {
+            // Add a space at the beginning of odd-numbered rows
+            if (i % 2 == 1) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < x; j++) {
+
+		    System.out.print("*");
+                // Add a space between asterisks, except for the last one in the row
+                if (j < x - 1) {
+                    System.out.print(" "); 
+                }
+            }
+                // Add a space at the end of even-numbered rows
+            if (i % 2 == 0 && i < x - 1) {
+                System.out.print(" ");
+            }
+            
+            // Move to the next line to start a new row
+            System.out.println();
+        }
+    }
 }
